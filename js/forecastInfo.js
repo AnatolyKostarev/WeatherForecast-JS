1f98aa9501c564b4b5e0709f212928e7bae9d96f
Anatoly <anatolykostarev@gmail.com>
2021-11-05 16:10:10 +0300

    Add currenForecast func component

---
+++ js/forecastInfo.js
@@ -0,0 +1,13 @@
+"use strict";
+const $forecastData = document.querySelector(".js-data");
+
+export function currentForecast() {
+  let divInfo = document.createElement("div");
+  divInfo.classList.add("forecast__info");
+  divInfo.innerHTML = `
+  <p class="forecast_temperature">8&deg;C</p>
+  <p class="forecast__condition">windy</p>
+  <button class="forecast__button" type="submit">Wrong city?</button>
+`;
+  $forecastData.append(divInfo);
+}
