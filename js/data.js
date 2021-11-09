62d2151fd7a52f8ff06591378139a91df74b2ad5
Anatoly <anatolykostarev@gmail.com>
2021-11-09 13:35:12 +0300

    Add data with API keys

---
+++ js/data.js
@@ -0,0 +1,6 @@
+"use strict";
+
+export const WEATHER__API =
+  "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=1ae7597707c74c56a3268459c8aab147";
+export const GEO__LOCATION =
+  "https://geo.ipify.org/api/v2/country,city?apiKey=at_S16YNtLOVLuqEWvAWQT3uejhLNl9A&ipAddress=8.8.8.8";
