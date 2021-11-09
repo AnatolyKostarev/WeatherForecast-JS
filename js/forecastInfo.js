4104b35550f764fe9fc9687099b5630165f8f95d
Anatoly <anatolykostarev@gmail.com>
2021-11-09 13:35:47 +0300

    Add arg obj for function

--- js/forecastInfo.js
+++ js/forecastInfo.js
@@ -1,12 +1,16 @@
 "use strict";
 const $forecastData = document.querySelector(".js-data");
 
-export function currentForecast() {
+export function currentForecast(obj) {
   let divInfo = document.createElement("div");
   divInfo.classList.add("forecast__info");
   divInfo.innerHTML = `
-  <p class="forecast_temperature">8&deg;C</p>
-  <p class="forecast__condition">windy</p>
+  <p class="forecast_temperature">${
+    obj.current.temp < 0
+      ? Math.floor(obj.current.temp)
+      : Math.ceil(obj.current.temp)
+  }&deg;C</p>
+  <p class="forecast__condition">${obj.current.weather[0].description}</p>
   <button class="forecast__button" type="submit" data-current="wrong">Wrong city?</button>
 `;
   $forecastData.append(divInfo);
