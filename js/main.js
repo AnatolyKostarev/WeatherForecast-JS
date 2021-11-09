041bce47516c80ee648f13e7e33a2a393bebe760
Anatoly <anatolykostarev@gmail.com>
2021-11-09 13:36:20 +0300

    Add fetch for getting weather data

--- js/main.js
+++ js/main.js
@@ -1,6 +1,27 @@
 "use strict";
 import { currentForecast } from "./forecastInfo.js";
-import { forecastSearch } from "./forecastSearch.js";
+// import { preloader } from "./preloader.js";
+// import { forecastSearch } from "./forecastSearch.js";
+// import { WEATHER__API, GEO__LOCATION } from "./data.js";
 
-currentForecast();
+navigator.geolocation.getCurrentPosition(succsess);
+
+function succsess(position) {
+  let crd = position.coords;
+  let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${crd.latitude}&lon=${crd.longitude}&units=metric&lang=ru&exclude={part}&appid=1ae7597707c74c56a3268459c8aab147`;
+
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Geo request failture");
+      }
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      currentForecast(data);
+    });
+}
+
+// currentForecast();
 // forecastSearch();
