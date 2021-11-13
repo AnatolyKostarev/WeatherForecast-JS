814842454e13b829d6d9a8c411aff74452fa11a2
Anatoly <anatolykostarev@gmail.com>
2021-11-13 18:35:22 +0300

    Rename forecastInfo.js into currentForecast.js

--- js/currentForecast.js
+++ js/currentForecast.js
@@ -5,12 +5,12 @@ export function currentForecast(obj) {
   let divInfo = document.createElement("div");
   divInfo.classList.add("forecast__info");
   divInfo.innerHTML = `
-  <p class="forecast_temperature">${
-    obj.current.temp < 0
-      ? Math.floor(obj.current.temp)
-      : Math.ceil(obj.current.temp)
+  <p class="forecast__temperature">${
+    obj.temp < 0 ? Math.floor(obj.temp) : Math.ceil(obj.temp)
   }&deg;C</p>
-  <p class="forecast__condition">${obj.current.weather[0].description}</p>
+  <p class="forecast__condition">${obj.type}</p>
+  <img class="forecast__icon" src="
+  https://openweathermap.org/img/wn/${obj.icon}@2x.png"/>
   <button class="forecast__button" type="submit" data-current="wrong">Wrong city?</button>
 `;
   $forecastData.append(divInfo);
