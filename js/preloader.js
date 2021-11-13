9dad8bf5b666bbf66670a86a26786c2ec7d2066d
Anatoly <anatolykostarev@gmail.com>
2021-11-13 18:31:43 +0300

    Add preloader.js

--- js/preloader.js
+++ js/preloader.js
@@ -1,11 +1,11 @@
 "use strict";
-const $forecastData = document.querySelector(".js-data");
+const $preloader = document.querySelector(".content__main");
 
 export function preloader() {
   let divPreloader = document.createElement("div");
-  divPreloader.classList.add("lds-default");
+  divPreloader.classList.add("lds-spinner");
   divPreloader.innerHTML = `
   <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>`;
 
-  $forecastData.append(divPreloader);
+  $preloader.prepend(divPreloader);
 }
